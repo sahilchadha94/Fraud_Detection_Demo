3fe64e2d85a0dedb9a3cc14120b2a03d50afa516
sahilchadha94 <chadha.sahil94@gmail.com>
2023-09-11 01:32:09 +0530

    Added ML Model code with different models

--- ML_Model.py
+++ ML_Model.py
@@ -1,1 +1,10 @@
 print("adding the ml related code here")
+
+def model1():
+    print("adding code for the ML model 1")
+
+def model2():
+    print("adding code for the ML model 2")
+
+def model3():
+    print("adding code for the ML model 3")
